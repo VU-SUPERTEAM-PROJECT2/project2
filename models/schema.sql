e0421b49b63bf5f08ac049112cf08e426bd5140a
Zach S.L <zach.s.ledford@gmail.com>
2020-05-20 22:39:31 -0400

    quiz files and js

--- models/schema.sql
+++ models/schema.sql
@@ -1,17 +1,38 @@
 DROP DATABASE IF EXISTS project2_db;
 CREATE DATABASE project2_db;
 
-CREATE TABLE beer (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    beer_name VARCHAR(200) NOT NULL,
-    beer_type VARCHAR(200) NOT NULL,
-    brewery VARCHAR(200) NOT NULL
+USE projec2_db;
+
+CREATE TABLE beer
+(
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+    beer_name VARCHAR
+    (200) NOT NULL,
+    beer_type VARCHAR
+    (200) NOT NULL,
+    brewery VARCHAR
+    (200) NOT NULL
 );
 
-CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_nickname VARCHAR(200) NOT NULL,
-    user_favorite VARCHAR(200),
+    CREATE TABLE user
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT PRIMARY KEY,
+    user_nickname VARCHAR
+        (200) NOT NULL,
+    user_favorite VARCHAR
+        (200),
     user_beer_foreignKey INTEGER,
-    user_zipcode VARCHAR(5) NOT NULL
+    user_zipcode VARCHAR
+        (5) NOT NULL
+    user_score INTEGER,
+);
+
+        CREATE TABLE questions
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT PRIMARY KEY,
+    question VARCHAR
+            (255) NOT NULL,
 );
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,20 @@
-var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples,
+  //     });
+  //   });
+  // });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/quiz", function(req, res) {
+    // res.sendFile(path.join(__dirname, "../views/quiz.handlebars"));
+    res.render("quiz");
   });
 
   // Render 404 page for any unmatched routes
