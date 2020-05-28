97ba1de0ef5ca42c24b133d854e6a2f187c79240
Melanie Dutton <Princessmelanoke@gmail.com>
2020-05-27 20:54:33 -0500

    getting db password via process.env.MYSQL_PASSWORD

--- models/index.js
+++ models/index.js
@@ -7,14 +7,15 @@ var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
-
+console.log("process.env.MYSQL_PASSWORD: ", process.env.MYSQL_PASSWORD);
+console.log("config.password: ", config.password);
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(
     config.database,
     config.username,
-    config.password,
+    process.env.MYSQL_PASSWORD || config.password,
     config
   );
 }
