e0541392046674a15f292a1561d88ad3cc074b94
Vance Newberry <vancenewberry@gmail.com>
2020-05-29 15:54:55 -0500

    weak addition of trending beers from api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,14 +7,6 @@ module.exports = function(app) {
       res.json(dbQuizs);
     });
   });
-<<<<<<< HEAD
-  // // Create a new example
-  // app.post("/api/quiz", function(req, res) {
-  //   db.Example.create(req.body).then(function(dbExample) {
-  //     res.json(dbExample);
-  //   });
-  // });
-=======
 
   // Create a new example
   app.post("/api/quiz", function(req, res) {
@@ -22,7 +14,6 @@ module.exports = function(app) {
       res.json(dbQuizs);
     });
   });
->>>>>>> 2a5198cadbc3d9d7c8888db0b5bd5c558b1c2a99
 
   // // Delete an example by id
   // app.delete("/api/quiz/:id", function(req, res) {
