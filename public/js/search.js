eb2ef1f45d670166cf4c57784c9b3660daa84d56
Vance Newberry <vancenewberry@gmail.com>
2020-05-31 16:15:43 -0500

    search page js and renamed quiz.js to quiz-object.js and moved to public folder

--- public/js/search.js
+++ public/js/search.js
@@ -1,14 +1,31 @@
-//this code needs to be repurposed for use on a website it is currently not useable in the project but is the basics of querying from openbeerDB
-var axios = require("axios");
-// var env = process.env.NODE_ENV;
-var client_id = "84F4224C1925D7EA68A3F19E962B060CB2C5F3F2";
-var client_secret = "9125283B9252CB44CF87D885C80195D506971E3D";
-// We then run the request with axios module on a URL with a JSON
-axios
+// Get references to page elements
+var $searchWord = $("#search-word");
+var $submitBtn = $("#submit");
+var $resultList = $("#results-list");
 
-  .get(
-    `https://api.untappd.com/v4/beer/trending?client_id=${client_id}&client_secret=${client_secret}`
-  )
-  .then(function(response) {
-    console.log(response.data.response.micro.items[0].beer.beer_name);
-  });
+
+// // what happens when submit is pressed on search page
+var handleFormSubmit = function(event) {
+  event.preventDefault();
+
+//   // grabs value of the search form so that we can use it in our api request
+  var beerSearch = $searchWord.val().trim();
+
+  
+
+  if (!beerSearch) {
+    alert("You must enter a name of a beer");
+    return;
+  }
+
+  // API.saveExample(example).then(function() {
+  //   refreshExamples();
+//   // });
+$resultList.append(beerSearch);
+console.log(beerSearch);
+
+  $searchWord.val("");
+};
+
+// //event listener for submit button
+$submitBtn.on("click", handleFormSubmit);
