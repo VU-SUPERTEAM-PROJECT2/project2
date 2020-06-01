c74ebeff9eb6ff26c0c5fba86b96eb94b85e946a
Vance Newberry <vancenewberry@gmail.com>
2020-06-01 13:00:17 -0500

    added pokemon generator(not exporting correctly

--- public/js/pokemon.js
+++ public/js/pokemon.js
@@ -1,20 +1,30 @@
-var randomPokemon = function () {
-    // Grabbing and storing the data-animal property value from the button
-    var ranNum = Math.floor(Math.random() * 152);
-    // Constructing a queryURL using the random number
-    var queryURL = "https://pokeapi.co/api/v2/pokemon/" + ranNum;
-    console.log(queryURL);
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    })
-      // After data comes back from the request
-      .then(function (response) {
-        console.log("Response Object:", response);
-        console.log("Name:", response.forms[0].name);
-        var pokemonPic = "https://pokeres.bastionbot.org/images/pokemon/" + ranNum + ".png";
-        console.log("Picture url", pokemonPic);
-      });
-  };
-  randomPokemon();
+var axios = require("axios");
+var Pokemon = require("./nonSQL_models/pokemon");
+
+// Grabbing and storing the data-animal property value from the button
+var ranNum = Math.floor(Math.random() * 152);
+// Constructing a queryURL using the random number
+var queryURL = "https://pokeapi.co/api/v2/pokemon/" + ranNum;
+// console.log(queryURL);
+// Performing an AJAX request with the queryURL
+var pokemon;
+
+var generator = function(){axios
+
+  .get(queryURL)
+  // After data comes back from the request
+  .then(function(response) {
+    //   console.log("Response Object:", response);
+    // console.log("Name:", response.data.name);
+    var pokemonPic =
+      "https://pokeres.bastionbot.org/images/pokemon/" + ranNum + ".png";
+    //   console.log("Picture url", pokemonPic);
+    var newPokemon = new Pokemon(
+    response.data.name, 
+    pokemonPic);
+    pokemon = newPokemon;
+    
+  });
+}
+
+module.exports =pokemon;
---
+++ public/js/test.js
@@ -0,0 +1,4 @@
+var pokemon = require("./pokemon");
+console.log(pokemon);
+console.log(pokemon.name);
+console.log(pokemon.URL);
