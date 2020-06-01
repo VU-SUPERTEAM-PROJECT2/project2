9a3c672a07ec81721c909a56159f7025ec0aac75
Vance Newberry <vancenewberry@gmail.com>
2020-06-01 15:08:11 -0500

    reworked search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,33 +1,36 @@
-// Get references to page elements
-var $searchWord = $("#search-word");
-var $submitBtn = $("#submit");
-var $resultList = $("#results-list");
 
 
-// // what happens when submit is pressed on search page
-var handleFormSubmit = function(event) {
-  event.preventDefault();
+var axios = require("axios");
+var Beer = require("./nonSQL_models/beers");
+// We then run the request with axios module on a URL with a JSON
 
-//   // grabs value of the search form so that we can use it in our api request
-  var beerSearch = $searchWord.val().trim();
+// var env = process.env.NODE_ENV;
+var client_id = process.env.CLIENTID;
+var client_secret = process.env.CLIENTSECRET;
+// We then run the request with axios module on a URL with a JSON
+var beers = [];
 
-  
+module.exports = function searcher(searchterm) {
+  axios
 
-  if (!beerSearch) {
-    alert("You must enter a name of a beer");
-    return;
-  }
+  .get(
+    `https://api.untappd.com/v4/beer/trending?client_id=${client_id}&client_secret=${client_secret}`
+  )
+  .then(function(response) {
+    // console.log(response.data.response.micro.items[0].beer.beer_name);
+    for (i = 0; i < 3; i++) {
+      var newBeer = new Beer(
+        response.data.response.micro.items[i].beer.beer_name,
+        response.data.response.micro.items[i].beer.beer_style,
+        response.data.response.micro.items[i].brewery.brewery_name,
+        response.data.response.micro.items[i].beer.beer_label,
+        response.data.response.micro.items[i].brewery.contact.url
+      );
+      beers.push(newBeer);
+    }
+  });
 
-  // API.saveExample(example).then(function() {
-  //   refreshExamples();
-//   // });
-// querySearch(beerSearch);
-console.log(beerSearch);
-
-  $searchWord.val("");
+module.exports = function() {
+  return beers;
 };
-
-// //event listener for submit button
-$submitBtn.on("click", handleFormSubmit);
-
-
+}
